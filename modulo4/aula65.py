1c461433ca5e088d2c1e682cc72790a32d8e5a1d
Fernando <fscasas05@gmail.com>
2025-09-19 23:51:56 +0100

    exs: 65 & 66

---
+++ modulo4/aula65.py
@@ -0,0 +1,12 @@
+"""
+Introdução às funções (def) em python
+Funções são trechos de código que podem ser reutilizados
+Elas podem ou não receber entradas de dados e podem ou não retornar uma saída de dados
+Por padrão, funções retornam None (tipo NoneType)
+"""
+
+def main():
+    return 'Olá Mundo!'
+
+if __name__ == '__main__':
+    main()
