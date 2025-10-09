cd5f4d3bbcf928b1b07ac0268f2d3cd080c4d5f1
Fernando <fscasas05@gmail.com>
2025-10-09 17:22:30 +0100

    ex: 193

--- aula193/main.py
+++ aula193/main.py
@@ -3,6 +3,10 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 ROOT_FOLDER = Path(__file__).parent
 CHROMEDRIVER_EXEC = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
@@ -23,6 +27,7 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
     return browser
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
     # Example
     # options = '--headless', '--disable-gpu',
     options = ()
@@ -30,5 +35,37 @@ if __name__ == '__main__':
 
     # Como antes
     browser.get('https://www.google.com/')
+    
+    # Espera para encontrar o botão Aceitar tudo
+    try:
+        accept_button = WebDriverWait(browser, TIME_TO_WAIT).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '//button[.//div[text()="Aceitar tudo"]]')
+            )
+        )
+        sleep(5) 
+        accept_button.click()
+    except Exception as e:
+        print('Erro ao encontrar o botão Aceitar tudo', e)
+       
+    # Espera para encontrar input
+    try:
+        search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+            EC.presence_of_element_located(
+                (By.NAME, 'q')
+            )
+        )
 
-    sleep(10)    
+        search_input.send_keys('Hello World')
+        sleep(7)  
+        search_input.send_keys(Keys.ENTER)  
+
+        results = browser.find_element(By.ID, 'search')
+        links = results.find_element(By.TAG_NAME, 'a')
+        links[0].click()
+
+    except Exception as e:
+        print('Erro ao encontrar o input de busca', e)
+
+    # Sleep - apenas para visualização
+    sleep(TIME_TO_WAIT)    
