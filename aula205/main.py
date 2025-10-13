d5f1cb839116131c4dc6986c9feeabba1d05dbe2
Fernando <fscasas05@gmail.com>
2025-10-13 22:28:25 +0100

    ex: 205

--- aula205/main.py
+++ aula205/main.py
@@ -11,6 +11,15 @@ connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
 cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+# Create table
+cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
     'id INTEGER PRIMARY KEY AUTOINCREMENT,'
@@ -18,6 +27,16 @@ cursor.execute(
     'weight REAL'
     ')'
 )
+connection.commit()
+
+# Insert data
+sql = (
+    f'INSERT INTO {TABLE_NAME} (name, weight) '
+    'VALUES (?, ?)'
+)
+
+# cursor.execute(sql, ['Fernando', 59.5])
+cursor.executemany(sql, [['Fernando', 59.5], ['Maria', 65.0], ['João', 78.3]])
 connection.commit()
 
 cursor.close()
